4cdd2d64699e53f37ba6a5070f4edd4cccf71ead
francescocecca <cecca.francesco@yahoo.it>
2021-10-22 18:55:03 +0200

    Update GestioneBanca.java

--- src/GestioneBanca/GestioneBanca.java
+++ src/GestioneBanca/GestioneBanca.java
@@ -13,24 +13,38 @@ public class GestioneBanca {
 		String inp;
 		Utente[] utenti = new Utente[100];
 		TerminaleSportello terminale = new TerminaleSportello(utenti);
-//aggiungi metodo controlla
-		System.out.println("Quale operazione desidera fare?");
+		boolean login = false;
+		//aggiungi metodo controlla
+		while(login) {
+			System.out.println("Quale operazione desidera fare?");
+			inp=input.nextLine();
+			switch(inp) {
+				case "Login": if(terminale.login()) {
+					System.out.println("Login effettuato");
+					login = true;
+				}
+				case "Sign in": {
+					System.out.println("Inserisci username e password");
+					String newnome=input.nextLine();
+					String newpassword=input.nextLine();
+					Utente utente = new Utente(newnome, newpassword);
+					utenti[Utente.idUltimo] = utente;					
+				}
+			}
+		}
+		System.out.println("Quale operazione desidera fare sul suo conto?");
 		inp=input.nextLine();
 		switch(inp) {
-			case "Login": if(terminale.login()) {
-				System.out.println("Login effettuato");
-			}
-			case "Sign in": {
-				System.out.println("Inserisci username e password");
-				String newnome=input.nextLine();
-				String newpassword=input.nextLine();
-				Utente utente = new Utente(newnome, newpassword);
-				utenti[Utente.idUltimo] = utente;
-					
-				
-			}
-		
+			case "Apri Conto" : {
+				System.out.println("Inserisci il tuo nome");
+				String nome=input.nextLine();
+				System.out.println("Quanto vuoi versare?");
+				double saldo=input.nextDouble();
+				ContoCorrente cc= new ContoCorrente(nome,saldo) ;
+				}
+		 
 		}
+		
 	}
 
 }
