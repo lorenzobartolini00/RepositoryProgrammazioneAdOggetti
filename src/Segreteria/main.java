26bd962d8beaa2ee691bbc9ab64a59992fad7ff0
francescocecca <cecca.francesco@yahoo.it>
2021-11-03 18:28:16 +0100

    Aggiornamento Main

--- src/Segreteria/main.java
+++ src/Segreteria/main.java
@@ -8,37 +8,24 @@ public class main {
 		// TODO Auto-generated method stub
 		
 		Scanner input= new Scanner(System.in);
+		InOut inout = new InOut();
+		boolean exit =false;
+		
 		
 		//operazioni studente
 		
-		boolean gotStudente = false;
-		boolean exit = false;
-		
-		while(!exit)
-		{
-			// Prima di effettuare il login
-			while(!gotStudente && !exit) 
-			{
-				String listaOperazioniStudente = InOut.ottieniComando("Quale operazione desidera fare?", "listaOperazioniStudente");
+		while(!exit) {
+			
+			String risposta = input.nextLine();
+			switch(risposta) {
+			//cerca iscrivi ritira visul iscittti visul laurati
+			
+			case "Cerca"{
 				
-				// Chiede all'utente se vuole cercare uno studente o uscire
-				switch(listaOperazioniStudente) 
-				{
-				case "Cerca": 
-					{
-						if(gotStudente = InOut.gotStudente())System.out.println("Studente trovato."); 		//Chiama la funzione della classe terminale login, che ritorna true in caso di accesso effettuato, cioè nel caso in cui le credenziali inserite combacino con quelle di un utente inserito nella lista
-						else System.out.println("Studente non Trovato. Procedere con un nuovo tentativo.");		//Ritorna false nel caso in cui l'utente inserito non è presente nella lista di quelli che si sono registrati
-						break;
-					}
-				case "Exit" :
-				{
-					exit = true;
-					break;
-				}
 				
-				}
+				
 			}
-
+			}
+		}
 	}
-
 }
