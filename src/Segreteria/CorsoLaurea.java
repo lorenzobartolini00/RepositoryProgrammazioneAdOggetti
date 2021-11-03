fdb73210826823298edd5d05960cf760ba996bba
francescocecca <cecca.francesco@yahoo.it>
2021-11-03 17:33:16 +0100

    CorsoLaurea Aggiornamento

--- src/Segreteria/CorsoLaurea.java
+++ src/Segreteria/CorsoLaurea.java
@@ -8,21 +8,34 @@ public class CorsoLaurea {
 	//Attributi
 	
 	private String nome;
-	private ArrayList<Studente> listaStudenti;
+	private ArrayList<Studente> listaStudenti= new ArrayList<Studente>();
 	private int numCrediti;
 	
 	//Costruttore
-	CorsoLaurea(String nome, ArrayList<Studente> listaStudenti, int numCrediti){
+	CorsoLaurea(String nome, int numCrediti){
 		this.nome=nome;
-		this.listaStudenti=listaStudenti;
 		this.numCrediti=numCrediti;
 	}
 	
-	//getter
+	//Getter e Setter
 	public String getNome() { return this.nome; }
 	public int getNumCrediti() {return this.numCrediti;}
 	public ArrayList<Studente> getListaStudenti() {return this.listaStudenti;}
 	public void setNome(String nome) {this.nome=nome;}
 	public void setNumMinCrediti(int numCrediti) {this.numCrediti=numCrediti;}
 	public void setLista(ArrayList<Studente> listaStudenti) {this.listaStudenti=listaStudenti;}
+	
+	//Metodi
+	public void Iscrivere(Studente studente) {
+		boolean esito=false;
+		if (!listaStudenti.contains(studente)) 
+		{
+			esito=listaStudenti.add(studente);
+		}
+	}
+	
+	
+	
+	
+
 }
