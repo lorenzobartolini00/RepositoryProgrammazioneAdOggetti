20d965b0f142fb3191dfeaf6edb444afb29c92fb
lorenzobartolini00 <s1092626@studenti.univpm.it>
2021-11-03 17:33:01 +0100

    Databse

--- src/Segreteria/Database.java
+++ src/Segreteria/Database.java
@@ -7,36 +7,38 @@ public class Database {
 	
 	Database()
 	{
-		CorsoLaurea ingegneria = new CorsoLaurea();
-		CorsoLaurea economia = new CorsoLaurea();
-		CorsoLaurea medicina = new CorsoLaurea();
-		CorsoLaurea scienze = new CorsoLaurea();
-		CorsoLaurea agraria = new CorsoLaurea();
+		CorsoLaurea ingegneria = new CorsoLaurea("Ingegneria", 120);
+		CorsoLaurea economia = new CorsoLaurea("Economia", 120);
+		CorsoLaurea medicina = new CorsoLaurea("Medicina", 120);
+		CorsoLaurea scienze = new CorsoLaurea("Scienze", 120);
+		CorsoLaurea agraria = new CorsoLaurea("Agraria", 120);
 		
 		ArrayList<Esame> esami_ingengeria = new ArrayList<Esame>();
 		ArrayList<Esame> esami_economia = new ArrayList<Esame>();
 		
 		Esame analisi1 = new Esame("Analisi 1", 9);
-		Esame geometria = new Esame();
-		Esame elDiInformatica = new Esame();
-		Esame analisi2 = new Esame();
-		Esame fondDiElettromagnetismo = new Esame();
-		Esame fisica = new Esame();
-		Esame elettrotecnica = new Esame();
-		Esame economiaDellImpresa = new Esame();
-		Esame metodiMatematici = new Esame();
-		Esame fisicaSuperiore = new Esame();
-		Esame chimica = new Esame();
-		Esame elDiElettronica = new Esame();
-		Esame teoriaDeiSegnali = new Esame();
-		Esame elDiControlliAutomatici = new Esame();
-		Esame telecomunicazioni = new Esame();
-		Esame elettronicaAnalogica = new Esame();
-		Esame misureElettroniche = new Esame();
-		Esame sistemiElettronici = new Esame();
-		Esame elettronicaDigitale = new Esame();
-		Esame programmazioneAdOggetti = new Esame();
-		Esame analisi1 = new Esame();
+		Esame geometria = new Esame("Geometria", 6);
+		Esame elDiInformatica = new Esame("Elementi di informatica", 9);
+		Esame analisi2 = new Esame("Analisi 2", 9);
+		Esame fondDiElettromagnetismo = new Esame("Fondamenti di elettromagnetismo", 9);
+		Esame fisica = new Esame("Fisica", 9);
+		Esame elettrotecnica = new Esame("Elettrotecnica", 9);
+		Esame economiaDellImpresa = new Esame("Economia dell'impresa", 9);
+		Esame metodiMatematici = new Esame("Metodi matematici", 9);
+		Esame fisicaSuperiore = new Esame("Fisica superiore", 9);
+		Esame chimica = new Esame("Chimica", 9);
+		Esame elDiElettronica = new Esame("Elementi di elettronica", 9);
+		Esame teoriaDeiSegnali = new Esame("Teoria dei segnali", 6);
+		Esame elDiControlliAutomatici = new Esame("Elementi di Controlli Automatici", 9);
+		Esame telecomunicazioni = new Esame("Telecomunicazioni", 9);
+		Esame elettronicaAnalogica = new Esame("Elettronica analogica", 9);
+		Esame misureElettroniche = new Esame("Misure elettroniche", 9);
+		Esame sistemiElettronici = new Esame("Sistemi elettronici", 6);
+		Esame elettronicaDigitale = new Esame("Elettronica digitale", 9);
+		Esame programmazioneAdOggetti = new Esame("Programmazione ad oggetti", 9);
+		Esame sistemiDiTelecomunicazioni = new Esame("Sistemi di telecomunicazioni", 9);
+		Esame CADSP = new Esame("Circuiti e algoritmi per il digital signal processing", 6);
+		
 		
 	}
 }
