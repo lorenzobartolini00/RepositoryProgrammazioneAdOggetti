5674fe0ba250ed6bbc9f5157bc252f47fe5acf8a
francescocecca <cecca.francesco@yahoo.it>
2021-11-09 17:00:57 +0100

    Creazione Classi
    Sono state create le classi Automobile e Optional

--- src/automobili/Automobile.java
+++ src/automobili/Automobile.java
@@ -1,5 +1,56 @@
 package automobili;
 
-public class Automobile {
+import java.util.Scanner;
 
+//creo la superclasse automobile
+
+public class Automobile {
+	
+	Scanner in=new Scanner(System.in);
+
+	//attributi
+	
+	private String targa;
+	private String marca;
+	private String modello;
+	private Motore motore = new Motore();
+	private ArrayList<Optional> listaOptional=new ArrayList<Optional>();
+		
+	//getter e setter
+	public String getTarga() {
+		return targa;
+	}
+	public void setTarga(String targa) {
+		this.targa = targa;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModello() {
+		return modello;
+	}
+	public void setModello(String modello) {
+		this.modello = modello;
+	}
+	public Motore getMotore() {
+		return motore;
+	}
+	public void setMotore(Motore motore) {
+		this.motore = motore;
+	}
+	
+	//metodi
+	
+	//addOption deve prendere in ingresso un'istanza della classe Optional
+	public void addOptional() {
+		String codice = in.nextLine();
+		String descrizione = in.nextLine();
+		float valore = in.nextFloat();
+		Optional optional=new Optional(codice,descrizione,valore);
+		listaOptional.add(optional);
+	}
+	
 }
---
+++ src/automobili/Optional.java
@@ -0,0 +1,47 @@
+package automobili;
+
+public class Optional {
+	
+	//attributi
+	private String codice;
+	private String descrizione;
+	private float valore;
+	
+	//getter e setter
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public float getValore() {
+		return valore;
+	}
+
+	public void setValore(float valore) {
+		this.valore = valore;
+	}
+	
+	//costruttore
+	public Optional(String codice, String descrizione, float valore) {
+		this.codice=codice;
+		this.descrizione=descrizione;
+		this.valore=valore;
+	}
+	
+	//metodo
+	public String toString() {
+		return codice+descrizione+valore;
+	}
+
+}
