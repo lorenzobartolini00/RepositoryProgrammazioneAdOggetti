422a18901b2c913ab2bdeaba7c8f6a15fdaa1907
francescocecca <cecca.francesco@yahoo.it>
2021-11-09 17:26:06 +0100

    Creazione Classe Coupè

--- src/automobili/Fuoristrada.java
+++ src/automobili/Fuoristrada.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 import motori.Motore;
 
-public class Tipo_4x4 extends Automobile{
+public class Fuoristrada extends Automobile{
 	
 	private float altezzaSospensioni;
 	
-	public Tipo_4x4(float altezzaSospensioni,String targa, String marca, String modello, Motore motore, ArrayList<Optional> listaOptional) {
+	public Fuoristrada(String targa, String marca, String modello, Motore motore, ArrayList<Optional> listaOptional, float altezzaSospensioni) {
 		super(targa,marca,modello,motore,listaOptional);
 		this.altezzaSospensioni=altezzaSospensioni;
 	}
